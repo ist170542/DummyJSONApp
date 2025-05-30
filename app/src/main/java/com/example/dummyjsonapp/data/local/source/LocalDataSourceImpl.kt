255ca38b3d9d4fbd0ff1fd1323020ce5471c0d4b
DBaptista <duarte.baptista@tecnico.ulisboa.pt>
2025-05-30 18:55:42 +0100

    Local data source commit correction

---
+++ app/src/main/java/com/example/dummyjsonapp/data/local/source/LocalDataSourceImpl.kt
@@ -0,0 +1,29 @@
+package com.example.dummyjsonapp.data.local.source
+
+import com.example.dummyjsonapp.data.local.database.ProductDetailsDao
+import com.example.dummyjsonapp.data.local.database.ProductsDao
+import com.example.dummyjsonapp.data.local.entities.ProductDetailsEntity
+import com.example.dummyjsonapp.data.local.entities.ProductEntity
+
+class LocalDataSourceImpl(
+    private val productsDao: ProductsDao,
+    private val productDetailsDao: ProductDetailsDao
+) : LocalDataSource {
+
+    override suspend fun getProducts(): List<ProductEntity> {
+        return productsDao.getProducts()
+    }
+
+    override suspend fun insertProducts(products: List<ProductEntity>) {
+        productsDao.insertProducts(products)
+    }
+
+    override suspend fun getProductDetails(id: Int): ProductDetailsEntity {
+        return productDetailsDao.getProductDetails(id)
+    }
+
+    override suspend fun insertProductDetails(productDetails: List<ProductDetailsEntity>) {
+        productDetailsDao.insertProductsDetails(productDetails)
+    }
+
+}
