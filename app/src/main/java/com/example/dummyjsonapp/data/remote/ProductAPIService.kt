7d78536ac9a9c5f93447ca62dc7028d9d64d6cf8
DBaptista <duarte.baptista@tecnico.ulisboa.pt>
2025-05-31 18:06:28 +0100

    Add the request to retrieve the products paginated from the service

--- app/src/main/java/com/example/dummyjsonapp/data/remote/ProductAPIService.kt
+++ app/src/main/java/com/example/dummyjsonapp/data/remote/ProductAPIService.kt
@@ -1,12 +1,20 @@
 package com.example.dummyjsonapp.data.remote
 
+import com.example.dummyjsonapp.data.remote.responses.PaginatedGetProductsResponseDto
 import com.example.dummyjsonapp.data.remote.responses.ProductDTO
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ProductAPIService {
 
     @GET("products")
     suspend fun getProducts(): Response<List<ProductDTO>>
+
+    @GET("products")
+    suspend fun getProductsPaginated(
+        @Query("skip") skip: Int,
+        @Query("limit") limit: Int
+    ): Response<PaginatedGetProductsResponseDto>
 
 }
