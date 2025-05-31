1944711f3b61fc3fd482fd6ffbee12bfb46f9e0f
DBaptista <duarte.baptista@tecnico.ulisboa.pt>
2025-05-31 18:09:22 +0100

    Add the logic to retrieve the products paginated (remote)

--- app/src/main/java/com/example/dummyjsonapp/data/remote/source/RemoteDataSource.kt
+++ app/src/main/java/com/example/dummyjsonapp/data/remote/source/RemoteDataSource.kt
@@ -1,7 +1,9 @@
 package com.example.dummyjsonapp.data.remote.source
 
+import com.example.dummyjsonapp.data.remote.responses.PaginatedGetProductsResponseDto
 import com.example.dummyjsonapp.data.remote.responses.ProductDTO
 
 interface RemoteDataSource {
     suspend fun getProducts(): List<ProductDTO>
+    suspend fun getProductsPaginated(skip: Int, limit: Int): PaginatedGetProductsResponseDto
 }
--- app/src/main/java/com/example/dummyjsonapp/data/remote/source/RemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/dummyjsonapp/data/remote/source/RemoteDataSourceImpl.kt
@@ -1,10 +1,12 @@
 package com.example.dummyjsonapp.data.remote.source
 
 import com.example.dummyjsonapp.data.remote.ProductAPIService
+import com.example.dummyjsonapp.data.remote.responses.PaginatedGetProductsResponseDto
 import com.example.dummyjsonapp.data.remote.responses.ProductDTO
 import retrofit2.HttpException
+import javax.inject.Inject
 
-class RemoteDataSourceImpl(
+class RemoteDataSourceImpl @Inject constructor(
     private val apiService: ProductAPIService
 ) : RemoteDataSource {
     override suspend fun getProducts(): List<ProductDTO> {
@@ -17,6 +19,19 @@ class RemoteDataSourceImpl(
             throw HttpException(response)
         }
 
+    }
+
+    override suspend fun getProductsPaginated(
+        skip: Int,
+        limit: Int
+    ): PaginatedGetProductsResponseDto {
+        val response = apiService.getProductsPaginated(skip = skip, limit)
+
+        if (response.isSuccessful) {
+            return response.body() ?: throw HttpException(response)
+        } else {
+            throw HttpException(response)
+        }
     }
 
 }
