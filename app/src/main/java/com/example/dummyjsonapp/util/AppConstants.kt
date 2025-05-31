3f0843c1a3e9fe12ce80fbe1b394694dcde68852
DBaptista <duarte.baptista@tecnico.ulisboa.pt>
2025-05-31 18:13:35 +0100

    Add empty string to the AppConstants

--- app/src/main/java/com/example/dummyjsonapp/util/AppConstants.kt
+++ app/src/main/java/com/example/dummyjsonapp/util/AppConstants.kt
@@ -3,4 +3,5 @@ package com.example.dummyjsonapp.util
 object AppConstants {
     const val BASE_URL = "https://dummyjson.com/"
     const val APP_DATABASE_NAME = "dummy_json_app_database"
+    const val EMPTY_STRING = ""
 }
