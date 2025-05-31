809268383e4be7c6edbc94d9c62eb0af2806c91b
DBaptista <duarte.baptista@tecnico.ulisboa.pt>
2025-05-31 18:05:50 +0100

    Network DI module corection (missing Retrofit :) )

--- app/src/main/java/com/example/dummyjsonapp/di/NetworkModule.kt
+++ app/src/main/java/com/example/dummyjsonapp/di/NetworkModule.kt
@@ -3,14 +3,30 @@ package com.example.dummyjsonapp.di
 import com.example.dummyjsonapp.data.remote.ProductAPIService
 import com.example.dummyjsonapp.data.remote.source.RemoteDataSource
 import com.example.dummyjsonapp.data.remote.source.RemoteDataSourceImpl
+import com.example.dummyjsonapp.util.AppConstants
+import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 /**
  * Hilt Module to provide API and Remote data source
  */
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(AppConstants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 
     @Singleton
     @Provides
